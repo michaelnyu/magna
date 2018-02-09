0e36369844edfd93dd20ffa7e37dce8b8b52bdd3
Michael Yu <michael.yu2016@gmail.com>
2018-02-09 15:32:52 -0800

    added frontend for testing

---
+++ magna-testing-frontend/index.js
@@ -0,0 +1,35 @@
+var apibase = "http://localhost:8000/api/v1/"
+
+async function sendData(){
+	var name = document.querySelector('#name').value;
+	var text = document.querySelector('#text').value;
+	var donation = Number(document.querySelector('#donation').value);
+
+	try {
+		const response = await fetch(apibase+"entries/", {
+			method: 'POST',
+			headers: {
+				'Content-Type': "application/json",
+			},
+			body: JSON.stringify({
+				"name": name,
+				"text": text,
+				"donation": donation,
+			})
+		});
+		const status = await response.status;
+		if (status >= 200 && status < 300) {
+			console.log("success");
+		}else{
+			throw new Error(status);
+		}
+
+	}catch(e){
+		throw new Error(e.message);
+	}
+}
+
+
+var submit_button = document.querySelector('#submit');
+submit_button.addEventListener('click', ()=>{ sendData() });	
+
